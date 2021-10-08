646a2dc58e60c3281c94b0b4e4778561ecf88b54
kda <UpkLk9It>
2021-10-08 12:00:39 +0500

    The bot with keybord

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import vk_api
 import random
 import pymysql
@@ -5,56 +6,77 @@ import pymysql
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 from pymysql.cursors import DictCursor
+from dotenv import load_dotenv
 
-token = "135b460f05ba32607385aeaf2bc9d44653feccb50626694edb020c5455c58ac05b266bffcee0829ec784f"
+load_dotenv()
+token = os.getenv('token')
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
+
+
 connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='password',
-    db='60971xxx',
+    host=os.getenv('host'),
+    user=os.getenv('user'),
+    password=os.getenv('password'),
+    db=os.getenv('db'),
     cursorclass=DictCursor
 )
+print(os.getenv('password'))
 
-with connection:
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM rating")
 
-    rows = cur.fetchall()
-
-    for row in rows:
-        print("{0} {1} {2}".format(row['id'], row['name'], row['description']))
+# with connection:
+#     cur = connection.cursor()
+#     cur.execute("SELECT * FROM employee")
+#
+#     rows = cur.fetchall()
+#
+#     for row in rows:
+#         print("{0} ФИО: {1} {2}".format(row['id'], row['name'], row['phone']))
 
 def send_msg(user_id, message, keyboard='{}'):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 1000), "keyboard":keyboard})
 
 # Основной цикл
-for event in longpoll.listen():
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button("Сотрудники",color=VkKeyboardColor.PRIMARY, payload={"button": "1"})
-    keyboard.add_button("Отделы", color=VkKeyboardColor.PRIMARY, payload={"button": "2"})
-    keyboard = keyboard.get_keyboard()
-    # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
+with connection:
+    for event in longpoll.listen():
+        keyboard = VkKeyboard(one_time=True)
+        keyboard.add_button("Сотрудники", color=VkKeyboardColor.PRIMARY, payload={"button": "1"})
+        keyboard.add_button("Отделы", color=VkKeyboardColor.PRIMARY, payload={"button": "2"})
+        keyboard = keyboard.get_keyboard()
 
-        # Если оно имеет метку для меня( то есть бота)
-        if event.to_me:
-            # send_msg(event.user_id, "Пример клавиатуры",keyboard)
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
 
-            # Сообщение от пользователя
-            request = event.text
+            # Если оно имеет метку для меня( то есть бота)
+            if event.to_me:
+                # send_msg(event.user_id, "Пример клавиатуры",keyboard)
 
-            # логика ответа
-            if request == "привет":
-                send_msg(event.user_id, "Хай")
-            elif request == "пока":
-                send_msg(event.user_id, "Пока((")
+                # Сообщение от пользователя
+                request = event.text
 
-            # elif event.message['payload'] == '{"button":"2"}':
-            #     send_msg(event.user_id, "Кнопка 2")
-            # elif event.message['payload'] == '{"button":"2"}':
-            #     send_msg(event.user_id, "Кнопка 1")
 
-            send_msg(event.user_id, "Не поняла вашего ответа......")
+                # логика ответа
+                if request == "привет":
+                    send_msg(event.user_id, "Хай",keyboard)
+                elif request == "пока":
+                    send_msg(event.user_id, "Пока((",keyboard)
+
+                # elif event.message['payload'] == '{"button":"2"}':
+                #     send_msg(event.user_id, "Кнопка 2")
+                # elif event.message['payload'] == '{"button":"2"}':
+                #     send_msg(event.user_id, "Кнопка 1")
+                else:
+
+                        cur = connection.cursor()
+                        cur.execute("SELECT E.name EN, E.phone, D.name DN  FROM employee E,department D WHERE E.dep_id = D.id\
+                         and (E.name LIKE '%"+request+"%' OR D.name LIKE '%"+request+"%')")
+
+                        rows = cur.fetchall()
+
+                        for row in rows:
+                            # print("{0} ФИО: {1} {2}".format(row['id'], row['name'], row['phone']))
+                            send_msg(event.user_id, row['EN']+' '+row['phone']+' '+row['DN'], keyboard)
+
+                # else:
+                #     send_msg(event.user_id, "Не поняла вашего ответа......",keyboard)
 
