46dc6619093b0e29d957e7aa96d4f6f0d8e42a7d
kda <UpkLk9It>
2021-10-15 12:03:22 +0500

    fix procfile

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ with connection:
 
                 # Сообщение от пользователя
                 request = event.text
-                print (event)
-
+                user = vk.method("users.get",{"user_ids":event.user_id,"fields": "photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, domain, has_mobile, contacts, site, education, universities, schools, status, last_seen, followers_count, occupation, nickname, relatives, relation, personal, connections, exports, activities, interests, music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts, can_see_audio, can_write_private_message, can_send_friend_request, is_favorite, is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend, friend_status, career, military, blacklisted, blacklisted_by_me, can_be_invited_group"})[0]
+                print(user["first_name"])
                 # логика ответа
                 if request == "привет":
                     send_msg(event.user_id, "Хай",keyboard)
@@ -64,12 +64,21 @@ with connection:
                 # elif event.message['payload'] == '{"button":"2"}':
                 #     send_msg(event.user_id, "Кнопка 1")
                 else:
-
+                        cur = connection.cursor()
+                        cur.execute("SELECT *  FROM users WHERE vk_id="+str(event.user_id))
+                        n = cur.rowcount
+                        cur.close()
+                        if n == 0:
+                            cur = connection.cursor()
+                            cur.execute("INSERT INTO users (first_name, last_name, vk_id) VALUES (%s, %s, %s)",(user["first_name"],user["last_name"],user["id"]))
+                            connection.commit()
+                            cur.close()
                         cur = connection.cursor()
                         cur.execute("SELECT E.name EN, E.phone, D.name DN  FROM employee E,department D WHERE E.dep_id = D.id\
                          and (E.name LIKE '%"+request+"%' OR D.name LIKE '%"+request+"%')")
 
                         rows = cur.fetchall()
+                        cur.close()
                         print(rows)
                         for row in rows:
                             # print("{0} ФИО: {1} {2}".format(row['id'], row['name'], row['phone']))
